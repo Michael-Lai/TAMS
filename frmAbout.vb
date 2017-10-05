528f5a21e57a95a6cfe578af71b7a9b57fce8450
Michael-Lai <32386502+Michael-Lai@users.noreply.github.com>
2017-10-05 12:35:34 +0800

    Variable Name
    I made changes to the variable name.

--- frmAbout.vb
+++ frmAbout.vb
@@ -1,5 +1,5 @@
 ﻿Public NotInheritable Class frmAbout
-    Private Sub OKButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
+    Private Sub OKButton_Click123(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OKButton.Click
         Me.Close()
     End Sub
 
